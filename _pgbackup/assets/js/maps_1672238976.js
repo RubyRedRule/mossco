ee684855ba6a008246c7831b2ddc9664ac717818
Deirdre McPhillips <dmcphillips@tifcohotels.ie>
2023-01-15 12:43:13 +0000

    Remove _pgbackup

--- _pgbackup/assets/js/maps_1672238976.js
+++
@@ -1,9 +0,0 @@
-function initMap() {
-  new google.maps.Map(document.getElementById("map"), {
-    mapId: "52dcb2d4339dcfed",
-    center: { lat: 48.85, lng: 2.35 },
-    zoom: 12,
-  });
-}
-
-window.initMap = initMap;
